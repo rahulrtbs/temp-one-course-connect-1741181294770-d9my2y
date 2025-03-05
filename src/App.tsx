66463244705aac4446338fdb5e28a103b1eaa8db
rahulrtbs <rahul.raut@technobase.in>
2025-03-05 18:58:36 +0530

    Copy src/App.tsx from one-course-connect

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Layout from "./components/layout/Layout";
+import Index from "./pages/Index";
+import Users from "./pages/Users";
+import Courses from "./pages/Courses";
+import Community from "./pages/Community";
+import Reports from "./pages/Reports";
+import Support from "./pages/Support";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/community" element={<Community />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/support" element={<Support />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
